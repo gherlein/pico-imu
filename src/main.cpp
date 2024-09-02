a01588ecf1004e71048b88333abcf602b9ca2435
Greg Herlein <gherlein@herlein.com>
2024-09-02 16:57:31 -0700

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
     This code originates from the Getting started with Raspberry Pi Pico document
     https://datasheets.raspberrypi.org/pico/getting-started-with-pico.pdf
     CC BY-ND Raspberry Pi (Trading) Ltd
@@ -9,21 +9,12 @@
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 
-const uint LED_PIN = 25;
+#include <bno085.h>
 
-int main() {
+int main()
+{
     bi_decl(bi_program_description("PROJECT DESCRIPTION"));
-    
+
     stdio_init_all();
-
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    while(1) {
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 1);
-        puts("Hello World\n");
-        sleep_ms(1000);
-    }
+    run_bno085();
 }
