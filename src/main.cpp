a261085d33b69732daff3835c3e935ec52d8ab6a
Greg Herlein <gherlein@herlein.com>
2024-09-04 15:47:46 -0700

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 /*
-    This code originates from the Getting started with Raspberry Pi Pico document
-    https://datasheets.raspberrypi.org/pico/getting-started-with-pico.pdf
-    CC BY-ND Raspberry Pi (Trading) Ltd
+
 */
 
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
-
-#include <bno085_imu.h>
+#include "bno_common.h"
+#include "bno085_imu.h"
+#include "pico-i2clib.h"
 
 i2c_inst_t *i2c;
 
@@ -19,5 +18,6 @@ int main()
 
     stdio_init_all();
     i2c = pico_init_i2c(i2c_default, 100 * 1000, PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN);
-    scan_i2c(i2c);
-    run_bno085(i2c);
+    pico_scan_i2c(i2c);
+    // run_bno085(i2c);
+}
