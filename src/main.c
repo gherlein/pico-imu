fc46195d889aa721e51ff7dbf2b7c7b8d6826983
Greg Herlein <gherlein@herlein.com>
2024-09-04 20:26:50 -0700

    updated

--- src/main.c
+++ src/main.c
@@ -17,7 +17,8 @@ int main()
     bi_decl(bi_program_description("PROJECT DESCRIPTION"));
 
     stdio_init_all();
-    i2c = pico_init_i2c(i2c_default, 100 * 1000, PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN);
+//    i2c = pico_init_i2c(i2c_default, 100 * 1000, PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN);
+    i2c = pico_init_i2c(i2c_default, 100 * 1000, 16, 17);
     pico_scan_i2c(i2c);
     // run_bno085(i2c);
 }
